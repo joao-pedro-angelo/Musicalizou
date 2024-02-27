1091c096ed9546a49140795adc52ea3db5d420f4
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-02-27 15:59:37 -0300

    criação do repositório de usuários

---
+++ src/main/java/com/music/review/app/domain/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.music.review.app.domain.repositories;
+
+import com.music.review.app.domain.entities.users.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    User findByEmail(String email);
+}
