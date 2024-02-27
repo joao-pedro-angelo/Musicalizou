a839cd6132c78382026cd74191675c2956e2a74d
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-02-27 01:52:34 -0300

    Initial commit

---
+++ src/main/java/com/music/review/app/AppMusicReviewApplication.java
@@ -0,0 +1,13 @@
+package com.music.review.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMusicReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMusicReviewApplication.class, args);
+	}
+
+}
