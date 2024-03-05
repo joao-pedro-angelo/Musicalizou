693c5d7080790e71a9624319c7fb01afe9c01a0f
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-03-05 01:57:38 -0300

    retira métodos e imports não utilizados - repositories

--- src/main/java/com/music/review/app/domain/repositories/ArtistRepository.java
+++ src/main/java/com/music/review/app/domain/repositories/ArtistRepository.java
@@ -4,10 +4,7 @@ import com.music.review.app.domain.entities.artists.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ArtistRepository extends JpaRepository<Artist, Long> {
-    Optional<Artist> findArtistByName(String name);
     Artist getArtistByName(String name);
 }
--- src/main/java/com/music/review/app/domain/repositories/UserRepository.java
+++ src/main/java/com/music/review/app/domain/repositories/UserRepository.java
@@ -4,8 +4,6 @@ import com.music.review.app.domain.entities.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User getUserByEmail(String email);
