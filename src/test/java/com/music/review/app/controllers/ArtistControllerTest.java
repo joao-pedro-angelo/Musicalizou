4f943f77db9d72a7d5a65a8289fb56c0d37f1a56
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-03-05 01:49:07 -0300

    apaga testes - momentaneamente - criar nova db para testes

--- src/test/java/com/music/review/app/controllers/ArtistControllerTest.java
+++ src/test/java/com/music/review/app/controllers/ArtistControllerTest.java
@@ -1,74 +1,7 @@
 package com.music.review.app.controllers;
 
-import com.music.review.app.domain.entities.artists.dtos.ArtistCreateDTO;
-import com.music.review.app.domain.repositories.ArtistRepository;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.json.JacksonTester;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
-
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureJsonTesters
-public class ArtistControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
-    private ArtistRepository artistRepository;
-
-    @Autowired
-    private JacksonTester<ArtistCreateDTO> artistCreateDTOJacksonTester;
-
-    @Test
-    @Transactional
-    @DisplayName("Deve retornar 400 Bad Request ao criar um artista inválido")
-    void createInvalidArtist() throws Exception {
-        ArtistCreateDTO artistCreateDTO = new ArtistCreateDTO(null, "1990", "Brasil", "Biografia do artista");
-
-        var response = mockMvc.perform(post("/artists")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(artistCreateDTOJacksonTester.write(artistCreateDTO).getJson()))
-                .andReturn().getResponse();
-
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
-    }
-
-    @Test
-    @Transactional
-    @DisplayName("Deve retornar 400 Bad Request ao criar um artista com dados inválidos")
-    void createValidArtist() throws Exception {
-        ArtistCreateDTO artistCreateDTO = new ArtistCreateDTO("oi",
-                "2010", "Brasil", "UmaBio");
-
-        var response = mockMvc.perform(post("/artists")
-                        .contentType("application/json")
-                        .content(artistCreateDTOJacksonTester.write(artistCreateDTO).getJson()))
-                .andReturn().getResponse();
-
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
-    }
-
-    @Test
-    @Transactional
-    @DisplayName("Deve retornar 404 Not Found ao buscar um artista inexistente")
-    void getNonExistentArtist() throws Exception {
-        var response = this.mockMvc.perform(get("/artists/name/naoexisto"))
-                .andReturn().getResponse();
-
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
-    }
+class ArtistControllerTest {
 }
--- src/test/java/com/music/review/app/controllers/ReviewControllerTest.java
+++ src/test/java/com/music/review/app/controllers/ReviewControllerTest.java
@@ -1,53 +1,7 @@
 package com.music.review.app.controllers;
 
-import com.music.review.app.domain.entities.reviews.dtos.ReviewCreateDTO;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.json.JacksonTester;
-import org.springframework.http.HttpStatus;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureJsonTesters
-public class ReviewControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private JacksonTester<ReviewCreateDTO> reviewCreateDTOJacksonTester;
-
-    @Test
-    @DisplayName("Deve devolver código 400 - Informações Inválidas")
-    void createReviewInvalidData() throws Exception{
-        var response = this.mockMvc.perform(post("/reviews"))
-                .andReturn().getResponse();
-
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
-    }
-
-    @Test
-    @Transactional
-    @DisplayName("Criação válida de review")
-    void createReviewValidData() throws Exception{
-        ReviewCreateDTO reviewCreateDTO = new ReviewCreateDTO("Comment", "Canudinho");
-
-        var response = this.mockMvc.perform(post("/reviews")
-                .contentType("application/json")
-                .content(this.reviewCreateDTOJacksonTester.write(reviewCreateDTO).getJson()))
-                .andReturn().getResponse();
-
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
-    }
-
-
+class ReviewControllerTest {
 }
