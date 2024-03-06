9d4f9ea6b5cb4eebe9f5c5866a244d6ce46ad35a
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-03-06 19:18:28 -0300

    nova migration - V9

---
+++ src/main/resources/db/migration/V9__Insert-Into-users-Table-userTest.sql
@@ -0,0 +1,6 @@
+-- User padrão para logar e testar
+-- A senha é '123456' encriptada
+
+INSERT INTO users (email, password) VALUES
+        ('basicUser@gmail.com', '$2a$12$8D2hmZ9VdCTUf4D.KHEefOeeQB5Gt9b/Gwm8FAKqL33ucZDQL2vty')
+;
