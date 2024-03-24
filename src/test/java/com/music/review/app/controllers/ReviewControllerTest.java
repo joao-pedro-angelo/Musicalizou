db43f93d2b31debf69c8ee1db4d0def1c7250bbb
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-03-24 18:29:14 -0300

    refatora testes - review controller

--- src/test/java/com/music/review/app/controllers/ReviewControllerTest.java
+++ src/test/java/com/music/review/app/controllers/ReviewControllerTest.java
@@ -48,31 +48,40 @@ class ReviewControllerTest {
 
     @BeforeEach
     void setUp(){
-        this.artist = new Artist(null, "Nome do Artista", "1968", "Brasil", "Bio");
-        this.artistRepository.save(artist);
-        this.music = new Music(null, "Nome da musica", MusicGen.OUTRO, artist);
-        this.musicRepository.save(music);
+        Artist artist = new Artist
+                (null, "Nome do Artista", "1968", "Brasil", "Bio");
+        this.artist = this.artistRepository.save(artist);
+
+        Music music = new Music
+                (null, "Nome da musica", MusicGen.OUTRO, this.artist);
+        this.music = this.musicRepository.save(music);
     }
 
     @Test
     @DisplayName("Tenta criar uma review para uma música que não existe - código 404")
-    void createReviewForNonExistingMusic() throws Exception {
-        ReviewCreateDTO reviewCreateDTO = new ReviewCreateDTO("MusicaInexistente", "Comentário da Review");
+    void createReviewFailed() throws Exception {
+        ReviewCreateDTO reviewCreateDTO = new ReviewCreateDTO
+                ("MusicaInexistente", "Comentário da Review");
 
         this.mockMvc.perform(post("/v1/reviews")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(this.reviewCreateDTOJacksonTester.write(reviewCreateDTO).getJson()))
+                        .content(this.reviewCreateDTOJacksonTester
+                                .write(reviewCreateDTO)
+                                .getJson()))
                 .andExpect(status().isNotFound());
     }
 
     @Test
     @DisplayName("Cria review válida - Código 201 Created")
-    void createValidReview() throws Exception{
-        ReviewCreateDTO reviewCreateDTO = new ReviewCreateDTO("Comment", this.music.getNameMusic());
+    void createReviewSucess() throws Exception{
+        ReviewCreateDTO reviewCreateDTO = new ReviewCreateDTO
+                ("Comment", this.music.getNameMusic());
 
         this.mockMvc.perform(post("/v1/reviews")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(this.reviewCreateDTOJacksonTester.write(reviewCreateDTO).getJson()))
+                        .content(this.reviewCreateDTOJacksonTester
+                                .write(reviewCreateDTO)
+                                .getJson()))
                 .andExpect(status().isCreated());
     }
 }
