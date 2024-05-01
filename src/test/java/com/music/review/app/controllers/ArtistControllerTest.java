17d89019a8d7c95a6f3d192f5338dfc5b45ee8c3
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-05-01 16:30:31 -0300

    atualiza testes de userController

--- src/test/java/com/music/review/app/controllers/ArtistControllerTest.java
+++ src/test/java/com/music/review/app/controllers/ArtistControllerTest.java
@@ -23,7 +23,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @SpringBootTest
--- src/test/java/com/music/review/app/controllers/UserControllerTest.java
+++ src/test/java/com/music/review/app/controllers/UserControllerTest.java
@@ -2,7 +2,6 @@ package com.music.review.app.controllers;
 
 import com.music.review.app.domain.entities.users.User;
 import com.music.review.app.domain.entities.users.dtos.UserCreateDTO;
-import com.music.review.app.domain.entities.users.dtos.UserUpdateDTO;
 import com.music.review.app.domain.repositories.UserRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,7 +22,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @SpringBootTest
@@ -40,13 +39,9 @@ class UserControllerTest {
     private JacksonTester<UserCreateDTO> userCreateDTOJacksonTester;
 
     @Autowired
-    private JacksonTester<UserUpdateDTO> userUpdateDTOJacksonTester;
-
-    @Autowired
     private UserRepository repository;
 
     private User user;
-    private String jsonExpected;
 
     @BeforeEach
     void setUp() {
@@ -55,10 +50,6 @@ class UserControllerTest {
                 new UserCreateDTO("oi@gmail.com", "password");
         User user = new User(userCreateDTO);
         this.user = this.repository.save(user);
-
-        this.jsonExpected = "{\"id\":"
-                + this.user.getId()
-                + ",\"email\":\"oi@gmail.com\"}";
     }
 
     @AfterEach
@@ -70,101 +61,54 @@ class UserControllerTest {
     @Test
     @DisplayName("Tenta cadastrar usuário com informações inválidas - código 400")
     void userCreateFailed() throws Exception {
-        var response = this.mockMvc.perform(post("/users"))
-                .andReturn().getResponse();
-
+        UserCreateDTO dto = new UserCreateDTO(
+                null, null);
+        var response = this.getResponsePost(dto);
         assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
     @Test
     @DisplayName("Cadastro de usuário com dados válidos - código 200")
     void userCreateSucess() throws Exception{
-        // Cria um DTO de criação de usuário com dados válidos
         UserCreateDTO userCreateDTO =
                 new UserCreateDTO("username@gmail.com", "password");
-
-        // Envia uma solicitação POST para /users com o DTO de criação de usuário
-        // e verifica a resposta
-        var response = this.mockMvc.perform(post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(this.userCreateDTOJacksonTester
-                                .write(userCreateDTO)
-                                .getJson())
-                )
-                .andReturn().getResponse();
-
-        // Verifica se o código de status HTTP é 200 (OK)
+        var response = this.getResponsePost(userCreateDTO);
         assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
     }
 
     @Test
     @DisplayName("Deve retornar o usuário encontrado pelo ID")
     void findByIdUserSucess() throws Exception {
-        // Faz uma solicitação GET para /users/id/{id} e verifica se a resposta tem status 200 (OK)
-        var response = this.mockMvc.perform(get("/users/id/{idUser}",
-                        this.user.getId())
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andReturn();
-
-        // Verifica se o conteúdo da resposta corresponde ao JSON esperado
-        assertThat(response.getResponse().getContentAsString())
-                .isEqualTo(this.jsonExpected);
+        var response = this.getResponseGetId(this.user.getId());
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
     }
 
     @Test
     @DisplayName("Tenta encontrar usuário não cadastrado - código 404")
     void findByIdUserFailed() throws Exception{
-        this.mockMvc.perform(get("/users/id/800")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNotFound());
+        var response = this.getResponseGetId(123L);
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
     }
 
-    @Test
-    @DisplayName("Deve retornar status 404 - Not Found ao buscar um usuário por email não existente")
-    void findByEmailFailed() throws Exception {
-        // Faz uma solicitação GET para /users/email/{email}
-        // e verifica se a resposta tem status 404 (Not Found)
-        mockMvc.perform(get("/users/email/emailnaoexistente@test.com")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNotFound());
-    }
 
-    @Test
-    @DisplayName("Deve retornar status 204 - No Content ao tentar excluir um usuário não existente")
-    void deleteUserFailed() throws Exception {
-        // Faz uma solicitação DELETE para /users/delete/1
-        // e verifica se a resposta tem status 200 (OK)
-        this.mockMvc.perform(delete("/users/900")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNoContent());
-    }
-
-    @Test
-    @DisplayName("Deve retornar status 404 - Not Found ao tentar atualizar um usuário não existente")
-    void updateUserFailed() throws Exception {
-        // Faz uma solicitação PUT para /users e verifica se a resposta tem status 404 (Not Found)
-        this.mockMvc.perform(put("/users")
+    private MockHttpServletResponse getResponsePost(
+            UserCreateDTO dto) throws Exception{
+        return this.mockMvc.perform(post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{ \"id\": 700, \"email\": \"novoemail@test.com\" }"))
-                .andExpect(status().isNotFound());
+                        .content(this.userCreateDTOJacksonTester
+                                .write(dto)
+                                .getJson())
+                )
+                .andReturn()
+                .getResponse();
     }
 
-    @Test
-    @DisplayName("Deve retornar status 200 - OK ao atualizar um usuário existente")
-    void updateUserSucess() throws Exception {
-        // Cria usuário para atualizar email
-        UserUpdateDTO userUpdateDTO = new UserUpdateDTO(this.user.getId(),
-                "novoemail@gmail.com", null);
-
-        // Requisição
-        var response = this.mockMvc.perform(put("/users")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(this.userUpdateDTOJacksonTester
-                        .write(userUpdateDTO)
-                        .getJson())
-        ).andReturn().getResponse();
-
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+    private MockHttpServletResponse getResponseGetId(
+            Long id) throws Exception{
+        return this.mockMvc.perform(get("/users/id/{idUser}",
+                        id)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andReturn()
+                .getResponse();
     }
 }
