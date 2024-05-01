8bad51de9b4bf4023b50aa35810824ae1d4e7080
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-05-01 16:12:56 -0300

    atualiza testes de artista controller

--- src/test/java/com/music/review/app/controllers/ArtistControllerTest.java
+++ src/test/java/com/music/review/app/controllers/ArtistControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -46,7 +47,6 @@ class ArtistControllerTest {
     private ArtistRepository artistRepository;
 
     private Artist artist;
-    private String jsonExpected;
 
     @BeforeEach
     void setUp() {
@@ -56,10 +56,6 @@ class ArtistControllerTest {
         artist.setCountry("Brasil");
         artist.setBio("Bio do artista");
         this.artist = this.artistRepository.save(artist);
-
-        this.jsonExpected =
-                "{\"name\":\"Artista Teste\",\"year\":\"2020\"," +
-                        "\"country\":\"Brasil\",\"bio\":\"Bio do artista\"}";
     }
 
     @AfterEach
@@ -71,67 +67,49 @@ class ArtistControllerTest {
     @Test
     @DisplayName("Tenta cadastrar artista com informações inválidas - código 400")
     void createArtistFailed() throws Exception {
-        var response = this.mockMvc.perform(post("/v1/artists"))
-                .andReturn().getResponse();
-
+        ArtistCreateDTO dto = new ArtistCreateDTO
+                (null , null,
+                        null, null);
+        var response = this.getResponsePost(dto);
         assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
     @Test
     @DisplayName("Cadastro de artista com dados válidos - código 201")
     void createArtistSucess() throws Exception {
-        ArtistCreateDTO artistCreateDTO = new ArtistCreateDTO
-                ("Artista Random", "2020", "Brasil", "Bio do artista");
-
-        var response = this.mockMvc.perform(post("/v1/artists")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(this.artistCreateDTOJacksonTester
-                                .write(artistCreateDTO)
-                                .getJson()))
-                .andReturn().getResponse();
-
+        ArtistCreateDTO dto = new ArtistCreateDTO
+                ("Artista Random", "2020",
+                        "Brasil", "Bio do artista");
+        var response = this.getResponsePost(dto);
         assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
     }
 
     @Test
     @DisplayName("Deve retornar o artista encontrado pelo ID")
     void findArtistByIdSucess() throws Exception {
-        var response = this.mockMvc.perform(get("/v1/artists/id/{id}",
-                        this.artist.getId())
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andReturn();
-
-        assertThat(response.getResponse().getContentAsString())
-                .isEqualTo(this.jsonExpected);
+        var response = this.getResponseGetId(this.artist.getId());
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
     }
 
     @Test
     @DisplayName("Tenta encontrar artista não cadastrado pelo ID - código 404")
     void findArtistByIdFailed() throws Exception {
-        this.mockMvc.perform(get("/v1/artists/id/1000")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNotFound());
+        var response = this.getResponseGetId(90812L);
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
     }
 
     @Test
     @DisplayName("Deve retornar o artista encontrado pelo nome")
     void findArtistByNameSucess() throws Exception {
-        var response = this.mockMvc.perform(get("/v1/artists/name/{name}",
-                        this.artist.getName())
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andReturn();
-
-        assertThat(response.getResponse().getContentAsString()).isEqualTo(this.jsonExpected);
+        var response = this.getResponseGetName(this.artist.getName());
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
     }
 
     @Test
     @DisplayName("Tenta encontrar artista não cadastrado pelo nome - código 404")
     void findArtistByNameFailed() throws Exception {
-        this.mockMvc.perform(get("/v1/artists/name/ArtistaInexistente")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNotFound());
+        var response = this.getResponseGetName("Not");
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
     }
 
     @Test
@@ -141,60 +119,86 @@ class ArtistControllerTest {
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andReturn();
-
         assertThat(response.getResponse().getContentAsString()).contains("Artista Teste");
     }
 
     @Test
     @DisplayName("Atualização de artista existente - código 200")
     void updateArtistSucess() throws Exception {
-        ArtistUpdateDTO artistUpdateDTO = new ArtistUpdateDTO(this.artist.getId(),
+        ArtistUpdateDTO dto = new ArtistUpdateDTO(this.artist.getId(),
                 "Artista Atualizado", "2021", "EUA", "Nova bio do artista");
-
-        var response = this.mockMvc.perform(put("/v1/artists")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(this.artistUpdateDTOJacksonTester
-                                .write(artistUpdateDTO)
-                                .getJson()))
-                .andReturn().getResponse();
-
+        var response = this.getResponsePut(dto);
         assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
     }
 
     @Test
     @DisplayName("Tenta atualizar artista não existente - código 404")
     void updateArtistFailed() throws Exception {
-        ArtistUpdateDTO artistUpdateDTO = new ArtistUpdateDTO(1000L,
+        ArtistUpdateDTO dto = new ArtistUpdateDTO(1000L,
                 "Artista Atualizado", "2021", "EUA", "Nova bio do artista");
-
-        var response = this.mockMvc.perform(put("/v1/artists")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(this.artistUpdateDTOJacksonTester
-                                .write(artistUpdateDTO)
-                                .getJson()))
-                .andReturn().getResponse();
-
+        var response = this.getResponsePut(dto);
         assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
     }
 
     @Test
     @DisplayName("Exclusão de artista existente - código 204")
     void deleteArtistSucess() throws Exception {
-        var response = this.mockMvc.perform(delete("/v1/artists/{id}",
-                        this.artist.getId())
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andReturn().getResponse();
-
+        var response = this.getResponseDelete(this.artist.getId());
         assertThat(response.getStatus()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 
     @Test
     @DisplayName("Tenta excluir artista não existente - código 204")
     void deleteArtistFailed() throws Exception {
-        var response = this.mockMvc.perform(delete("/v1/artists/1000")
+        var response = this.getResponseDelete(1000L);
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
+
+
+    private MockHttpServletResponse getResponseDelete(Long id) throws Exception{
+        return this.mockMvc.perform(delete("/v1/artists/{id}", id)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andReturn().getResponse();
+    }
 
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    private MockHttpServletResponse getResponsePut(
+            ArtistUpdateDTO dto) throws Exception{
+        return this.mockMvc.perform(put("/v1/artists")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(this.artistUpdateDTOJacksonTester
+                                .write(dto)
+                                .getJson()))
+                .andReturn().getResponse();
+    }
+
+    private MockHttpServletResponse getResponsePost(
+            ArtistCreateDTO dto) throws Exception{
+        return this.mockMvc.perform(post("/v1/artists")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(this.artistCreateDTOJacksonTester
+                                .write(dto)
+                                .getJson()))
+                .andReturn()
+                .getResponse();
+    }
+
+    private MockHttpServletResponse getResponseGetId(
+            Long id) throws Exception{
+        return this.mockMvc
+                .perform(get("/v1/artists/id/{id}",
+                        id)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andReturn()
+                .getResponse();
+    }
+
+    private MockHttpServletResponse getResponseGetName(
+            String name) throws Exception{
+        return this.mockMvc
+                .perform(get("/v1/artists/name/{name}",
+                        name)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andReturn()
+                .getResponse();
     }
 }
